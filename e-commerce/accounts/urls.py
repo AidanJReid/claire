0bf891e7eb552fa6ec360ef4efe104291140c37e
Aidan Reid <aidan.reid@hotmail.com>
2020-01-13 13:34:43 +0000

    settings changed

--- e-commerce/accounts/urls.py
+++ e-commerce/accounts/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url, include
-from . import urls_reset
-from .views import index, register, profile, logout, login
 from accounts.views import logout, login, registration, user_profile
 from accounts import url_reset
 

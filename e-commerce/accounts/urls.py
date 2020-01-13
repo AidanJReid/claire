f99a5692267b38e60ed6c4b1cf84269df37c2d94
Aidan Reid <aidan.reid@hotmail.com>
2020-01-13 14:16:24 +0000

    few edits to settings.py files

--- e-commerce/accounts/urls.py
+++ e-commerce/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration, user_profile
-from accounts import url_reset
+from . import urls_reset
+from .views import index, register, profile, logout, login
 
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
-    url(r'^register/', registration, name="registration"),
+    url(r'^register/', register, name="register"),
     url(r'^profile/', user_profile, name="profile"),
-    url(r'^password-reset/', include(url_reset))
+    url(r'^password-reset/', include(urls_reset))
 ]

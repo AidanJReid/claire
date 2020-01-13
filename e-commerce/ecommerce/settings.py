1daa65dcc872cb9bf5089f1d70725834c4599a5e
Aidan Reid <aidan.reid@hotmail.com>
2020-01-13 13:08:00 +0000

    updated settings

--- e-commerce/ecommerce/settings.py
+++ e-commerce/ecommerce/settings.py
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'accounts.backends.CaseInsensitiveAuth'
     'accounts.backends.EmailAuth'
 ]
 

86e901d6a97103dd01e4261ac6b5febdd757d4b7
Aidan Reid <aidan.reid@hotmail.com>
2020-01-14 13:25:40 +0000

    Search still not working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration, user_profile
+from accounts.views import logout, login, registration, user_profile, index
 from accounts import url_reset
 
 urlpatterns = [

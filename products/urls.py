bdc8570747c478165a95d4dc03678735c163a3c5
Aidan Reid <aidan.reid@hotmail.com>
2020-02-12 17:42:42 +0000

    More styling | Will upgrade to Django 2.x in next update

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_products, Detail
+from .views import all_products, detail
 
 urlpatterns = [
-    url(r'^$', all_products, name='products'),
-    url(r'detail/<product.id>', Detail.as_view(), name="detail"),
+    url(r'', all_products, name='products'),
+    url(r'^(?P<id>\d+)', detail, name='detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,32 +1,35 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product
-from django.views.generic import DetailView
+
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
-class Detail(DetailView):
-    """
-    Detailed view of the treatment for
-    interested parties to understand more
-    and add to cart
-    """
-    model = Product
-    template_name = "product-detail.html"
-    context_object_name = 'product'
-    extra_context = {}
 
-    def get_object(self, queryset=Product):
-        _id = self.kwargs.get('pk')
-        instance = get_object_or_404(Product, id=_id)
+def detail(request, id):
+    products = Products
+    return render(request, "product-detail.html")
 
-        self.extra_context['product'] = instance
-        return instance
 
-    def post(self, request):
-        _id = self.get('product.name')
-        instance = get_object_or_404(Product, id=_id)
-        product = self.get_object()
-        return render(request("product-detail.html", {{product.id}}))
+# class Detail(DetailView):
+#     """
+#     Detailed view of the treatment for
+#     interested parties to understand more
+#     and add to cart
+#     """
+#     model = Product
+#     template_name = 'product-detail.html'
+#     context_object_name = 'product'
+
+#     def get_object(self, queryset=Product):
+#         _id = self.kwargs.get('id')
+#         instance = get_object_or_404(Product, id=_id)
+#         return instance
+    
+#     def post(self, request, *args, **kwargs):
+#         _id = self.kwargs.get('id')
+#         instance = get_object_or_404(Product, id=_id)
+#         product = self.get_object()
+#         return redirect(request('product-detail.html', kwargs={product}))

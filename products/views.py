aedb926da1c98ccb320d6994a33999867b74f53a
Aidan Reid <aidan.reid@hotmail.com>
2020-04-01 11:04:40 +0000

    updated media query for mobile responsiveness - smaller devices

--- products/views.py
+++ products/views.py
@@ -8,28 +8,28 @@ def all_products(request):
     return render(request, "products.html", {"products": products})
 
 
-def detail(request, id):
-    products = Product.get_object(id)
-    return render(request, "product-detail.html", {"products": products})
+# def detail(request, id):
+#     products = Product.get_object(id)
+#     return render(request, "product-detail.html", {"products": products})
 
 
-# class Detail(DetailView):
-#     """
-#     Detailed view of the treatment for
-#     interested parties to understand more
-#     and add to cart
-#     """
-#     model = Product
-#     template_name = 'product-detail.html'
-#     context_object_name = 'product'
+class detail():
+    """
+    Detailed view of the treatment for
+    interested parties to understand more
+    and add to cart
+    """
+    model = Product
+    template_name = 'product-detail.html'
+    context_object_name = 'product'
 
-#     def get_object(self, queryset=Product):
-#         _id = self.kwargs.get('id')
-#         instance = get_object_or_404(Product, id=_id)
-#         return instance
+    # def detail(self, queryset=Product):
+    #     _id = self.kwargs.get('id')
+    #     instance = get_object_or_404(Product, id=_id)
+    #     return instance
     
-#     def post(self, request, *args, **kwargs):
-#         _id = self.kwargs.get('id')
-#         instance = get_object_or_404(Product, id=_id)
-#         product = self.get_object()
-#         return redirect(request('product-detail.html', kwargs={product}))
+    def get_object(self, request, *args, **kwargs):
+        _id = self.kwargs.get('id')
+        instance = get_object_or_404(Product, id=_id)
+        product = self.get_object()
+        return render(request('product-detail.html', kwargs))
